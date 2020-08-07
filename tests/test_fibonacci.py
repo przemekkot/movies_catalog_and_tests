d81a59174fb0a7acbb3b2ed6f5b5c87d5f5e1388
Przemyslaw Kot <przemekkot@wp.pl>
2020-08-07 21:55:55 +0200

    Add 2 fibonacci files

---
+++ tests/test_fibonacci.py
@@ -0,0 +1,14 @@
+import pytest
+import fibonacci
+from fibonacci import get_fibonacci_number
+
+@pytest.mark.parametrize('n, result', (
+   (0, 0),
+   (1, 1),
+   (2, 1),
+   (3, 2),
+   (10, 55),
+   (15, 610)
+))
+def test_fibonacci(n, result):
+   assert get_fibonacci_number(n) == result
