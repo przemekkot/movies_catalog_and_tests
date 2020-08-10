95cd18307f97fc1d9f8b0211dc5eb85081b4c391
Przemyslaw Kot <przemekkot@wp.pl>
2020-08-10 22:01:15 +0200

    Fix code in test_main.py, test results are now proper

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,20 +6,20 @@ from unittest.mock import Mock
 
 
 @pytest.mark.parametrize('n, result', (
-   ('movie/top_rated', 200),
-   ('movie/upcoming', 200),
-   ('movie/popular', 200),
-   ('movie/now_playing', 200)
+   ('top_rated', 200),
+   ('upcoming', 200),
+   ('popular', 200),
+   ('now_playing', 200)
 ))
 def test_homepage(monkeypatch, n, result):
    api_mock = Mock(return_value={'results': []})
    monkeypatch.setattr("tmdb_client.call_tmdb_api", api_mock)
 
    with app.test_client() as client:
-       response = client.get('/')
+       response = client.get(f'/?list_type={n}')
        #assert response.status_code == 200
        assert response.status_code == result
-       #api_mock.assert_called_once_with(n)
+       api_mock.assert_called_once_with(f'movie/{n}')
 
 
 
